17126c1a85877827114d3e046f28a53e708efba4
Unknown <equipomangoctin@gmail.com>
2018-04-13 10:35:03 -0500

    app
    casi lista

---
+++ lib/Controller/PageController.php
@@ -0,0 +1,48 @@
+<?php
+namespace OCA\Editor\Controller;
+
+use OCP\IRequest;
+use OCP\AppFramework\Http\ContentSecurityPolicy;
+use OCP\AppFramework\Http\TemplateResponse;
+use OCP\AppFramework\Http\DataResponse;
+use OCP\AppFramework\Controller;
+
+class PageController extends Controller {
+	private $userId;
+
+	public function __construct($AppName, IRequest $request, $UserId){
+		parent::__construct($AppName, $request);
+		$this->userId = $UserId;
+	}
+
+	/**
+	 * CAUTION: the @Stuff turns off security checks; for this page no admin is
+	 *          required and no CSRF check. If you don't know what CSRF is, read
+	 *          it up in the docs or you might create a security hole. This is
+	 *          basically the only required method to add this exemption, don't
+	 *          add it to any other method if you don't exactly know what it does
+	 *
+	 * @NoAdminRequired
+	 * @NoCSRFRequired
+	 */
+	public function index() {
+		$tmpl = new TemplateResponse($this->appName, 'index');//, [
+         //   'appName'            => $this->appName,
+        //], '');
+
+        $policy = new ContentSecurityPolicy();
+        $policy->addAllowedChildSrcDomain('*');
+        $policy->addAllowedFrameDomain('*');
+        $policy->addAllowedScriptDomain('*');
+        $policy->allowInlineStyle(true);
+        $policy->addAllowedStyleDomain('*');
+        $policy->addAllowedFontDomain('*');
+        $tmpl->setContentSecurityPolicy($policy);
+
+
+
+       return $tmpl;
+		//return new TemplateResponse('editor', 'index');  // templates/index.php
+	}
+
+}
---
+++ templates/navigation/index.php
@@ -0,0 +1,34 @@
+<ul>
+	<li class="editing">
+	    <a href="#" class="icon-folder">Folder entry</a>
+	    <div class="app-navigation-entry-utils">
+	        <ul>
+	            <li class="app-navigation-entry-utils-menu-button">
+	                <button class="icon-rename"></button>
+	            </li>
+	        </ul>
+	    </div>
+	    <div class="app-navigation-entry-edit">
+	        <form>
+	            <input type="text" value="Folder entry">
+	            <input type="submit" value="" class="icon-close">
+	            <input type="submit" value="" class="icon-checkmark">
+	        </form>
+	    </div>
+	</li>
+	<li class="collapsible open">
+
+    <!-- This is optional -->
+    <button class="collapse"></button>
+
+    <a href="#" class="icon-folder">Folder collapsed menu</a>
+    <ul>
+        <li><a href="#">Simple entry</a></li>
+        <li><a href="#">Simple entry</a></li>
+        <li><a href="#">Simple entry</a></li>
+        <li>
+            <a class="icon-folder" href="#">Simple folder</a>
+        </li>
+    </ul>
+</li>
+</ul>
